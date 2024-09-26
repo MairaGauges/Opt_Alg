be8cedded7c4159ce0038876a2f1cd94b0d7895f
Kine Vinningland <kinevinningland@Kines-MacBook-Pro.local>
2024-09-26 14:52:55 +0100

    test

--- task_6.m
+++ task_6.m
@@ -1,6 +1,6 @@
 load("classifier_dataset.mat", "testdataset", "testlabels", "traindataset", "trainlabels")
 
-%bla test 
+%bla test bhd
 
 n = 784; %vector size for each image
 rho = 0.1; % try different values
